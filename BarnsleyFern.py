984aa59555b3e737da1e1e6705cb393161ba8fea
lktn <140223005+lktn@users.noreply.github.com>
2024-10-08 15:14:24 +0700

    Update and rename 1.py to BarnsleyFern.py

--- BarnsleyFern.py
+++ BarnsleyFern.py
@@ -3,25 +3,11 @@ import numpy as np
 import random
 config.pixel_height = 2880
 config.pixel_width = 1620
-class trinagle_fractal(MovingCameraScene):
-    def construct(self):
-        self.camera.frame.save_state()
-        frame=self.camera.frame
-        tri = Triangle(color=WHITE)
-        for i in range(6):
-            t = VGroup(*[tri.copy() for _ in range(3)])
-            t[1].next_to(t[0], RIGHT, buff=0)
-            new_t = VGroup(t[0], t[1])
-            t[2].next_to(new_t, UP, buff=0)
-            self.play(frame.animate.move_to(t.get_center()).set_height(t.get_height()*1.5), rate_func=linear)
-            self.add(t)
-            tri = t
-
 class BarnsleyFern(Scene):
     def construct(self):
         x, y = 0, 0
         v = VGroup()
-        for _ in range(200000):
+        for _ in range(100000):
             r = random.random()
             if r < 0.01:
                 a = +0
