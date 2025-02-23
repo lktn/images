a740441c5b0f39ffa5d1c1a2c63d036e0f27d861
lktn <140223005+lktn@users.noreply.github.com>
2025-02-23 21:56:30 +0700

    Update MultibrotSet.py

--- MultibrotSet.py
+++ MultibrotSet.py
@@ -1,6 +1,6 @@
 from manim import *
-config.pixel_height = 2880*2
-config.pixel_width = 2880*2
+config.pixel_height = 5760
+config.pixel_width = 5760
 class MultibrotSet(Scene):
     def construct(self):
         d = 7
@@ -9,17 +9,13 @@ class MultibrotSet(Scene):
         a = (d-1)*d**(d/(1-d))
         b = 2**(1/(d-1))
         m = 2*b/n
-        def f(x, y, d, a, b):
-            c = complex(x, y)
-            if abs(c) > b:
-                return False
-            elif abs(c) < a:
-                return True
+        def f(c, d, a, b):
+            if abs(c) > b: return False
+            if abs(c) < a: return True
             z = c
             for _ in range(100):
                 z = z**d + c
-                if abs(z) > 2:
-                    return False
+                if abs(z) > 2: return False
             return True
 
         g = []
@@ -27,9 +23,8 @@ class MultibrotSet(Scene):
             for j in range(n+1):
                 x = i*m-b
                 y = j*m-b
-                if f(x, y, d, a, b):
-                    g.append([x, y])
+                if f(complex(x, y), d, a, b): g.append([x, y, 0])
                     
-        self.add(VGroup(*[Dot(point=[x, y, 0], radius=0.002) for x, y in g], 
+        self.add(VGroup(*[Dot(point=p, radius=0.002) for p in g], 
                 Circle(radius=b, stroke_width=0.5, color=WHITE)).scale(3.5),
                 MathTex(f"Area\\approx{4*b**2*len(g)/n**2:.10f}").move_to(-6*UP))
